b70420bcadc5ad7b2da03ce56337203e0de03d4d
TotoGarneau <98351865+TotoGarneau@users.noreply.github.com>
2022-10-10 14:02:15 -0400

    Update vueCarreRouge.py

--- vueCarreRouge.py
+++ vueCarreRouge.py
@@ -5,7 +5,7 @@ class VueCarreRouge():
     def __init__(self, root) :
         self.root = root
         self.canvas = tk.Canvas(root, background='white')
-        self.carreRouge = Carre(self.canvas, Vecteur(225, 225), 50, remplissage= 'red', bordure= 'black', epaisseur= 1)
+        self.carreRouge = Carre(self.canvas, Vecteur(225, 225), 50, 0, remplissage= 'red', bordure= 'black', epaisseur= 1)
 
     def dessiner(self) :
         self.carreRouge.draw()

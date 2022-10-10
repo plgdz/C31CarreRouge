f4c09c2c701e1842e57c868ab417a92e7d8f966e
unknown <nancy.wlodarski@outlook.com>
2022-10-10 15:26:44 -0400

    aire de jeu réajustée

--- vues.py
+++ vues.py
@@ -7,9 +7,9 @@ class VueJeu():
         self.root = root
         self.canvas = tk.Canvas(root, background="lightgrey", width=700, height=700)
         #Carré noir en arrière plan correspondant à la bordure du jeu 
-        self.carreBackground = Carre(self.canvas, Vecteur(350, 350), 500, 0, remplissage="black", bordure="black", epaisseur=0)
+        self.carreBackground = Carre(self.canvas, Vecteur(350, 350), 450, 0, remplissage="black", bordure="black", epaisseur=0)
         #Aire de jeu blanc où il est possible de déplacer le carré rouge 
-        self.carreAireJeu = Carre(self.canvas, Vecteur(350, 350), 450, 0, remplissage="white", bordure="black", epaisseur=0)
+        self.carreAireJeu = Carre(self.canvas, Vecteur(350, 350), 400, 0, remplissage="white", bordure="black", epaisseur=0)
         
     def dessiner(self, root) :   
         self.carreBackground.draw()

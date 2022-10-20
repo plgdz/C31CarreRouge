fe0aadc08d7dc989e5b470b111938f57b0b46601
TotoGarneau <98351865+TotoGarneau@users.noreply.github.com>
2022-10-20 14:15:14 -0400

    Update vueCarreRouge.py

--- vueCarreRouge.py
+++ vueCarreRouge.py
@@ -4,8 +4,9 @@ from c31Geometry2 import *
 class VueCarreRouge():
     def __init__(self, root) :
         self.root = root
-        self.canvas = tk.Canvas(root, background='white')
-        self.carreRouge = Carre(self.canvas, Vecteur(225, 225), 50, 0, remplissage= 'red', bordure= 'black', epaisseur= 1)
+        self.canvas = tk.Canvas(root, background='white', width= 450, height= 450)
+        self.carreRouge = Carre(self.canvas, Vecteur(225, 225), 50, remplissage= 'red', bordure= 'black', epaisseur= 1)
 
     def dessiner(self) :
         self.carreRouge.draw()
+        self.canvas.grid()

26244574cae770dc43da5fc5aab3c3259c2440c1
Veryba <maximegoaler@yahoo.com>
2022-11-01 14:50:35 -0400

    Premier Commit

---
+++ ControlleurCarreRouge.py
@@ -0,0 +1,15 @@
+
+from vues import vueCarreRouge
+
+class ControlleurCarreRouge :
+
+    def __init__(self, root, vues):
+        self.vues = VueCarreRouge(root)
+
+    def SetPosition(self):
+
+
+    def SetTaille(self):
+
+
+    def SetMouvement(self):

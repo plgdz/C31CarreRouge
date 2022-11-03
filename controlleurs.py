a968d2852ed0d10a9d3639bc2364029bc448eb4d
unknown <nancy.wlodarski@outlook.com>
2022-11-03 18:45:34 -0400

    controleur classement corrigé

--- controlleurs.py
+++ controlleurs.py
@@ -36,17 +36,13 @@ class ClassementControler:
 
     def __getMenuClassement__(self):
         return self.vues.canvas   
-
-    def ecrireScore(self, nom, secondes):   
-        secondes = "%.2f" % secondes    #Convertir le nb de secondes (float) en 2 décimales après la virgule  
+   
+    def ajouterAuClassement(self, nom, secondes): 
+        #secondes = "%.2f" % secondes    #Convertir le nb de secondes (float) en 2 décimales après la virgule  
         date = datetime.today().strftime('%Y-%m-%d')        # Enregistrer la date du jour 
         with open('fichierHighScore.csv', 'a', newline="") as csv_file:  #Ouvrir le fichier en "append" pour ajouter donnée
             writer = csv.writer(csv_file, delimiter=',')              
             writer.writerows(zip([nom], [secondes], [date]))             #Écrire le nom, nb de secondes et date dans le CSV
-
-
-    def ajouterAuClassement(self, nom, secondes): 
-        ClassementControler.ecrireScore(self, nom, secondes) 
 
         # Ouvrir le fichier CSV et le trier par le nombre de secondes des joueurs en ordre décroissant 
         data = csv.reader(open('fichierHighScore.csv'),delimiter=',')
@@ -56,7 +52,7 @@ class ClassementControler:
         sorted_csv_file = open('fichierHighScore.csv', 'w+', newline='') 
         write = csv.writer(sorted_csv_file)           
         for eachline in data:
-                write.writerow(eachline)  
+                write.writerow(eachline)   
 
 
     def supprimerScore(self, index):

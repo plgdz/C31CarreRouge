8c5f017ce3cb59afd8ee3fd413efc44a04e3d82a
Maxime <maximegoaler@yahoo.com>
2022-11-03 16:32:07 -0400

    Vue Carre

--- vueCarreRouge.py
+++ vueCarreRouge.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from c31Geometry2 import *
+from c31Geometry import *
 
 class VueCarreRouge():
     def __init__(self, root) :
         self.root = root
         self.canvas = tk.Canvas(root, background='white', width= 450, height= 450)
-        self.carreRouge = Carre(self.canvas, Vecteur(225, 225), 50, remplissage= 'red', bordure= 'black', epaisseur= 1)
+        self.carreRouge = Carre(self.canvas, Point(225, 225), 40, fill= 'red', outline= 'red', width= 1)
 
     def dessiner(self) :
         self.carreRouge.draw()
